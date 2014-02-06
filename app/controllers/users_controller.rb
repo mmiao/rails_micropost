664cca7c7dcfd4b0b91a5f88217d79f155c0a05d
mmiao <miaomy2008@gmail.com>
2014-02-06 00:10:50 -0500

    finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
       #render 'show'
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+       :password_confirmation)
     end
 
 end
