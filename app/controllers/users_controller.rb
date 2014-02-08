a43267bfce31956ea095556acda70318f12201e0
mmiao <miaomy2008@gmail.com>
2014-02-08 17:28:17 -0500

    section 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def new
   	@user = User.new
   end
@@ -17,7 +19,20 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  	#@user = User.find(params[:email]) why this does work?
+  end
+
+  def edit
+    #@user = User.find(params[:id])
+  end
+
+  def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private # private keyword doesn't end, make sure no public method after it
@@ -25,6 +40,15 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
        :password_confirmation)
+    end
+     # Before filters
+    def signed_in_user
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 end
