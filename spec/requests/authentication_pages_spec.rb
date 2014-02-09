11e12d709b93876548433e159491b7f485961672
mmiao <miaomy2008@gmail.com>
2014-02-09 00:34:59 -0500

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,6 +20,7 @@ describe "Authentication" do
 
 			it { should have_title(user.name) }
 			it { should have_link('Profile',     href: user_path(user)) }
+			it { should have_link('Users',       href: users_path) }
 			it { should have_link('Settings',    href: edit_user_path(user)) }
 			it { should have_link('Sign out',    href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
@@ -60,9 +61,25 @@ describe "Authentication" do
 					before { patch user_path(user) }
 					specify { expect(response).to redirect_to(signin_path) }
 				end
+
+				describe "visiting the user index" do
+					before { visit users_path }
+					it { should have_title('Sign in') }
+				end
 			end
 		end
+		
+		describe "as non-admin user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:non_admin) { FactoryGirl.create(:user) }
 
+			before { sign_in non_admin, no_capybara: true }
+
+			describe "submitting a DELETE request to the Users#destroy action" do
+				before { delete user_path(user) }
+				specify { expect(response).to redirect_to(root_url) }
+			end
+		end
 		describe "as wrong user" do
 			let(:user) { FactoryGirl.create(:user) }
 			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
